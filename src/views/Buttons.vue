af91a68f2d3c812285b13ff348c4c8e51ed44401
gitkyla <agitsyang@gmail.com>
2021-01-01 03:49:51 +0700

    move all content to the wrapper

--- src/views/Buttons.vue
+++ src/views/Buttons.vue
@@ -1,114 +1,116 @@
 <template>
-  <h1>Buttons</h1>
+  <div class="init-container">
+    <h1>Buttons</h1>
 
-  <component-layout>
-    <Item msg="&lt;Button /&gt;">
-      <Button></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button /&gt;">
+        <Button></Button>
+      </Item>
 
-    <Item msg="&:hover, &:focus">
-      <Button demo="default"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&:hover, &:focus">
+        <Button demo="default"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button variant='outline' /&gt;">
-      <Button variant="outline"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button variant='outline' /&gt;">
+        <Button variant="outline"></Button>
+      </Item>
 
-    <Item msg="&:hover, &:focus">
-      <Button variant="outline" demo="outline"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&:hover, &:focus">
+        <Button variant="outline" demo="outline"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button variant='text' /&gt;">
-      <Button variant="text"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button variant='text' /&gt;">
+        <Button variant="text"></Button>
+      </Item>
 
-    <Item msg="&:hover, &:focus">
-      <Button variant="text" demo="text"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&:hover, &:focus">
+        <Button variant="text" demo="text"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button disableShadow /&gt;">
-      <Button disableShadow></Button>
-    </Item>
-    <Item msg="&lt;Button disableShadow color='primary' /&gt;">
-      <Button disableShadow color="primary"></Button>
-    </Item>
-  </component-layout>
+    <component-layout>
+      <Item msg="&lt;Button disableShadow /&gt;">
+        <Button disableShadow></Button>
+      </Item>
+      <Item msg="&lt;Button disableShadow color='primary' /&gt;">
+        <Button disableShadow color="primary"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button disabled /&gt;">
-      <Button disabled></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button disabled /&gt;">
+        <Button disabled></Button>
+      </Item>
 
-    <Item msg="&lt;Button variant='text' disabled /&gt;">
-      <Button variant="text" disabled></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Button variant='text' disabled /&gt;">
+        <Button variant="text" disabled></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button startIcon='code' /&gt;">
-      <Button startIcon="code" color="primary"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button startIcon='code' /&gt;">
+        <Button startIcon="code" color="primary"></Button>
+      </Item>
 
-    <Item msg="&lt;Button endIcon='code'/&gt;">
-      <Button endIcon="code"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Button endIcon='code'/&gt;">
+        <Button endIcon="code"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button size='sm'/&gt;">
-      <Button size="sm"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button size='sm'/&gt;">
+        <Button size="sm"></Button>
+      </Item>
 
-    <Item msg="&lt;Button size='md'/&gt;">
-      <Button size="md"></Button>
-    </Item>
+      <Item msg="&lt;Button size='md'/&gt;">
+        <Button size="md"></Button>
+      </Item>
 
-    <Item msg="&lt;Button size='lg'/&gt;">
-      <Button size="lg"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Button size='lg'/&gt;">
+        <Button size="lg"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Button color='default'/&gt;">
-      <Button color="default"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&lt;Button color='default'/&gt;">
+        <Button color="default"></Button>
+      </Item>
 
-    <Item msg="&lt;Button color='primary'/&gt;">
-      <Button color="primary"></Button>
-    </Item>
+      <Item msg="&lt;Button color='primary'/&gt;">
+        <Button color="primary"></Button>
+      </Item>
 
-    <Item msg="&lt;Button color='secondary'/&gt;">
-      <Button color="secondary"></Button>
-    </Item>
+      <Item msg="&lt;Button color='secondary'/&gt;">
+        <Button color="secondary"></Button>
+      </Item>
 
-    <Item msg="&lt;Button color='danger'/&gt;">
-      <Button color="danger"></Button>
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Button color='danger'/&gt;">
+        <Button color="danger"></Button>
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&:hover, &:focus">
-      <Button color="default" demo="default"></Button>
-    </Item>
+    <component-layout>
+      <Item msg="&:hover, &:focus">
+        <Button color="default" demo="default"></Button>
+      </Item>
 
-    <Item>
-      <Button color="primary" demo="primary"></Button>
-    </Item>
+      <Item>
+        <Button color="primary" demo="primary"></Button>
+      </Item>
 
-    <Item>
-      <Button color="secondary" demo="secondary"></Button>
-    </Item>
+      <Item>
+        <Button color="secondary" demo="secondary"></Button>
+      </Item>
 
-    <Item>
-      <Button color="danger" demo="danger"></Button>
-    </Item>
-  </component-layout>
+      <Item>
+        <Button color="danger" demo="danger"></Button>
+      </Item>
+    </component-layout>
+  </div>
 </template>
 
 <script>
--- src/views/Inputs.vue
+++ src/views/Inputs.vue
@@ -1,94 +1,96 @@
 <template>
-  <h1>Inputs</h1>
+  <div class="init-container">
+    <h1>Inputs</h1>
 
-  <!-- Input Default -->
-  <component-layout>
-    <Item msg="&lt;Input /&gt;">
-      <Input />
-    </Item>
+    <!-- Input Default -->
+    <component-layout>
+      <Item msg="&lt;Input /&gt;">
+        <Input />
+      </Item>
 
-    <Item msg="&:hover">
-      <Input demo="hover" />
-    </Item>
+      <Item msg="&:hover">
+        <Input demo="hover" />
+      </Item>
 
-    <Item msg="&:focus">
-      <Input demo="focus" />
-    </Item>
-  </component-layout>
+      <Item msg="&:focus">
+        <Input demo="focus" />
+      </Item>
+    </component-layout>
 
-  <!-- Input Error -->
-  <component-layout>
-    <Item msg="&lt;Input error /&gt;">
-      <Input error />
-    </Item>
+    <!-- Input Error -->
+    <component-layout>
+      <Item msg="&lt;Input error /&gt;">
+        <Input error />
+      </Item>
 
-    <Item msg="&:hover">
-      <Input demo="hover" />
-    </Item>
+      <Item msg="&:hover">
+        <Input demo="hover" />
+      </Item>
 
-    <Item msg="&:focus">
-      <Input error demo="error" />
-    </Item>
-  </component-layout>
+      <Item msg="&:focus">
+        <Input error demo="error" />
+      </Item>
+    </component-layout>
 
-  <!-- Input Disabled -->
-  <component-layout>
-    <Item msg="&lt;Input disabled /&gt;">
-      <Input disabled />
-    </Item>
-  </component-layout>
+    <!-- Input Disabled -->
+    <component-layout>
+      <Item msg="&lt;Input disabled /&gt;">
+        <Input disabled />
+      </Item>
+    </component-layout>
 
-  <!-- Input helperText -->
-  <component-layout>
-    <Item msg="&lt;Input helperText='Wrong Password' /&gt;">
-      <Input helperText="Wrong Password" />
-    </Item>
+    <!-- Input helperText -->
+    <component-layout>
+      <Item msg="&lt;Input helperText='Wrong Password' /&gt;">
+        <Input helperText="Wrong Password" />
+      </Item>
 
-    <Item msg="&lt;Input helperText='Wrong Password' error /&gt;">
-      <Input helperText="Wrong Password" error />
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Input helperText='Wrong Password' error /&gt;">
+        <Input helperText="Wrong Password" error />
+      </Item>
+    </component-layout>
 
-  <!-- Input startIcon & endIcon -->
-  <component-layout>
-    <Item msg="&lt;Input startIcon='call' /&gt;">
-      <Input startIcon="call" />
-    </Item>
+    <!-- Input startIcon & endIcon -->
+    <component-layout>
+      <Item msg="&lt;Input startIcon='call' /&gt;">
+        <Input startIcon="call" />
+      </Item>
 
-    <Item msg="&lt;Input endIcon='lock' /&gt;">
-      <Input endIcon="lock" />
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Input endIcon='lock' /&gt;">
+        <Input endIcon="lock" />
+      </Item>
+    </component-layout>
 
-  <!-- Input Value -->
-  <component-layout>
-    <Item msg="&lt;Input value='text' /&gt;">
-      <Input value="text" />
-    </Item>
-  </component-layout>
+    <!-- Input Value -->
+    <component-layout>
+      <Item msg="&lt;Input value='text' /&gt;">
+        <Input value="text" />
+      </Item>
+    </component-layout>
 
-  <!-- Input Size -->
-  <component-layout>
-    <Item msg="&lt;Input size='sm' /&gt;">
-      <Input size="sm" />
-    </Item>
+    <!-- Input Size -->
+    <component-layout>
+      <Item msg="&lt;Input size='sm' /&gt;">
+        <Input size="sm" />
+      </Item>
 
-    <Item msg="&lt;Input size='md' /&gt;">
-      <Input size="md" />
-    </Item>
-  </component-layout>
+      <Item msg="&lt;Input size='md' /&gt;">
+        <Input size="md" />
+      </Item>
+    </component-layout>
 
-  <component-layout display="block">
-    <Item msg="&lt;Input fullWidth /&gt;">
-      <Input fullWidth />
-    </Item>
-  </component-layout>
+    <component-layout display="block">
+      <Item msg="&lt;Input fullWidth /&gt;">
+        <Input fullWidth />
+      </Item>
+    </component-layout>
 
-  <component-layout>
-    <Item msg="&lt;Input multiline row='4' /&gt;">
-      <Input multiline row="4" />
-    </Item>
-  </component-layout>
+    <component-layout>
+      <Item msg="&lt;Input multiline row='4' /&gt;">
+        <Input multiline row="4" />
+      </Item>
+    </component-layout>
+  </div>
 </template>
 
 <script>
